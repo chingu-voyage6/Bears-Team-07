278a2713a245e0d590a86880b42010e01db4a823
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-06-21 19:19:23 -0500

    Dummy register and Login pages

---
+++ client/src/views/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="register text-center">
+    <WelcomeHeader/>
+    <body>
+      <p>Welcome to Register page</p>
+    </body>
+  </div>
+</template>
+
+<script>
+import WelcomeHeader from "@/components/WelcomeHeader.vue";
+
+export default {
+  name: "welcome-header",
+  components: {
+    WelcomeHeader
+  }
+};
+</script>
+
+<style scoped>
+body {
+  padding: 100px 20px 20px 20px;
+}
+</style>
