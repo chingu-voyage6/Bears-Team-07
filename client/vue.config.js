479af01a3a5789e393649fa03d7781f403d007e9
Fernando Ortiz <fortiz2k@gmail.com>
2018-06-22 22:20:22 -0500

    Revert "remove warnings and tweaks."
    This reverts commit 52f8e78308fb60304ecfb68251c833360870dc3b.

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,16 +1,16 @@
 // vue.config.js
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
-  chainWebpack: config => {
-    config.plugin("provide").use(webpack.ProvidePlugin, [
-      {
-        $: "jquery",
-        jquery: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-        Popper: ["popper.js", "default"]
-      }
-    ]);
-  }
+    chainWebpack: config => {
+        config
+            .plugin('provide')
+            .use(webpack.ProvidePlugin, [{
+                $: 'jquery',
+                jquery: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+                Popper: ['popper.js', 'default']
+            }]);
+    },
 };
