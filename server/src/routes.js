674376981093b0fc3ee356d761eaa5a9e7aabfe9
Fernando Ortiz <fortiz2k@gmail.com>
2018-06-28 23:14:42 -0500

    remove CORS from server.js they were already added to src/routes.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,10 @@ import usersRouter from './controllers/users/router';
 export default function routes(app) {
   app.use('/api/v1/auth', authRouter);
   app.use('/api/v1/users', usersRouter);
+
+  // Handling CORS by adding headers to the response
+  // so that the browser on the client allows interaction
+  // though both client and server are on different origins
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
