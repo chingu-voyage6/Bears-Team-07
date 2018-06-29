8bfaf9e6269c0fd4d544b071571a52563735154d
Fernando Ortiz <fortiz2k@gmail.com>
2018-06-28 19:04:07 -0500

    add custom CORS support

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,4 +4,13 @@ import usersRouter from './controllers/users/router';
 export default function routes(app) {
   app.use('/api/v1/auth', authRouter);
   app.use('/api/v1/users', usersRouter);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+      res.status(200).json({});
+    }
+    next();
+  });
 }
