fc11d62173ed36ada8de0a19f2a8a926e07cb16e
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-06-28 20:14:11 -0500

    Added joi validations for creating a user

--- server/src/schemas/userSchema.js
+++ server/src/schemas/userSchema.js
@@ -1,11 +1,69 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
+/*
 exports.createUserSchema = Joi.object({
   username: Joi.string().alphanum().min(2).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().required()
 });
+*/
+
+module.exports = {
+  createUserSchema(req, res, next) {
+    const schema = {
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      username: Joi.string().alphanum().min(2).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$'),
+      ),
+      admin: Joi.boolean().required(),
+    };
+    const {error, value} = Joi.validate(req.body, schema)
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'firstname':
+          res.status(400).send({
+            error: 'Please provide a First Name.',
+          });
+          break;
+        case 'lastname':
+          res.status(400).send({
+            error: 'Please provide a First Name.',
+          });
+          break;
+        case 'username':
+          res.status(400).send({
+            error: 'Username must have a minimum of 2 and a maximum of 30 characters consisting of alphabets and numerics',
+          });
+          break;
+        case 'email':
+          res.status(400).send({
+            error: 'Please provide a valid email address'
+          });
+          break;
+        case 'password':
+          res.status(400).send({
+            error: 'Password must contain only lower case, upper case and numerics with a minimum of 8 and a maximum of 32 characters in length.',
+          });
+          break;
+        case 'admin':
+          res.status(400).send({
+            error: 'Please provide either true or false.',
+          });
+          break;
+        default:
+          res.status(400).send({
+            error: error.details,
+          });
+      }
+    } else {
+      next();
+    }
+  }
+};
 
 exports.authenticateUserSchema = Joi.alternatives().try(
   Joi.object({
