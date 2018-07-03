f8043333cd6be92b587ee7e2b1ed4be174f759d2
Fernando Ortiz <nandub@users.noreply.github.com>
2018-07-02 23:24:55 -0500

    refactor utils, routes and add pino middleware logger. (#25)
    * refactor utils, routes and add pino middleware logger.
    * update readme

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,16 @@
 import authRouter from './controllers/auth/router';
 import usersRouter from './controllers/users/router';
-import { CORS } from './utils/util';
+import PinoLogger from 'express-pino-logger';
+import pino from './logger';
+import {
+  CORS
+} from './utils';
 
 export default function routes(app) {
+  app.use(CORS);
+  app.use(PinoLogger({
+    logger: pino
+  }));
   app.use('/api/v1/auth', authRouter);
   app.use('/api/v1/users', usersRouter);
-  app.use(CORS);
 }
