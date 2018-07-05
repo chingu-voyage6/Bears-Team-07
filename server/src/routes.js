7bcca9fd13f68db0b01bcd9e17ea143ccceb4f38
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-07-04 20:58:39 -0500

    Merging changes from development branch

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,23 +2,12 @@ import PinoLogger from 'express-pino-logger';
 import CORS from 'cors';
 import authRouter from './controllers/auth/router';
 import usersRouter from './controllers/users/router';
-<<<<<<< HEAD
-import PinoLogger from 'express-pino-logger';
-import pino from './logger';
-import {
-  CORS
-} from './utils';
-
-export default function routes(app) {
-  app.use(CORS);
-=======
 import pino from './logger';
 
 export default function routes(app) {
   app.use(CORS());
->>>>>>> development
   app.use(PinoLogger({
-    logger: pino
+    logger: pino,
   }));
   app.use('/api/v1/auth', authRouter);
   app.use('/api/v1/users', usersRouter);
