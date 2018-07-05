6820cf4fecad369f690d36754b6ded67be7b53b8
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-07-05 18:15:03 -0500

    importing vuex and vuex-router-sync to use them globally

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { store } from "@/store/store.js";
+import { sync } from "vuex-router-sync";
 
 Vue.config.productionTip = false;
+sync(store, router);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
