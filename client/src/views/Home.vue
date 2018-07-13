647e6157452dcd0d0848578ca30fc4be6e2e3536
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-07-13 16:11:32 -0500

    Added "Select File" and "Browse" buttons with initial functionality

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,25 +1,71 @@
 <template>
   <div class="home text-center">
-    <WelcomeHeader/>
     <body>
       <p>Welcome to home page</p>
+      <input
+        style="display:none"
+        type="file"
+        @change="onFileSelected"
+        ref="fileInput">
+      <button class="btn btn-custom" 
+        @click="$refs.fileInput.click()">
+        Select File
+      </button>
+      <button class="btn btn-custom"
+        @click="onUpload">
+        Upload
+      </button>
+      <p>{{ fileName }}</p>
     </body>
   </div>
 </template>
 
 <script>
-import WelcomeHeader from "@/components/WelcomeHeader.vue";
-
 export default {
-  name: "logout",
-  components: {
-    WelcomeHeader
+  name: "home",
+  data() {
+    return {
+      selectedFile: null,
+      fileName: null
+    };
+  },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      this.fileName = this.selectedFile.name;
+    },
+    onUpload() {
+      const fd = new FormData();
+      fd.append("upload", this.selectedFile);
+      fd.append("username", this.$store.state.user.username);
+      //hardcoding title and content as Samuel Urias is working on this part
+      fd.append("title", "test title");
+      fd.append("content", "test content");     
+      for (var pair of fd.entries()) {
+        console.log(pair[0] + ' , ' + pair[1]); 
+      }
+    }
   }
 };
 </script>
-
+ 
 <style scoped>
 .home {
   padding: 100px 20px 20px 20px;
+}
+.btn.btn-custom {
+  background-color: #b71c1c;
+  color: #fff;
+  margin-right: 10px;
+  font-size: 16px;
+  border: 1px solid #fbe9e7;
+  border-radius: 5px;
+  overflow: hidden;
+}
+.btn.btn-custom:hover {
+  background-color: #420906;
+}
+.btn.btn-custom > i {
+  padding-right: 2px;
 }
 </style>
