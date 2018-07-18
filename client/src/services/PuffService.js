26a5c0f60b05b7232030b88fd3927c9753067837
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-07-18 13:46:31 -0500

    Added an argument to view file upload progress in console

--- client/src/services/PuffService.js
+++ client/src/services/PuffService.js
@@ -2,6 +2,11 @@ import Api from "./Api.js";
 
 export default {
   createPuffWithImage(puffDetails) {
-    return Api().post("/api/v1/puffs/u", puffDetails);
+    return Api().post("/api/v1/puffs/u", puffDetails, {
+      onUploadProgress: uploadEvent => {
+        console.log("Upload progress: " + Math.round(uploadEvent.loaded / uploadEvent.total * 100) +
+            "%");
+      }
+    });
   }
 };
