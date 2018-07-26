ed802728bf717663879a2eb72d48d557dbd95965
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-07-25 19:57:41 -0500

    Added authorization header information while creating puffs

--- client/src/services/PuffService.js
+++ client/src/services/PuffService.js
@@ -7,15 +7,29 @@ export default {
       headers: { Authorization: tokenAuth }
     });
   },
-  createPuff(puffDetails) {
-    return Api().post("/api/v1/puffs/", puffDetails);
-  },
-  createPuffWithImage(puffDetails) {
-    return Api().post("/api/v1/puffs/u", puffDetails, {
-      onUploadProgress: uploadEvent => {
-        console.log("Upload progress: " + Math.round(uploadEvent.loaded / uploadEvent.total * 100) +
-            "%");
-      }
+  createPuff(puffDetails, userToken) {
+    let tokenAuth = "Bearer " + userToken;
+    return Api().post("/api/v1/puffs/", puffDetails, {
+      headers: { Authorization: tokenAuth }
     });
+  },
+  createPuffWithImage(puffDetails, userToken) {
+    let tokenAuth = "Bearer " + userToken;
+    return Api().post(
+      "/api/v1/puffs/u",
+      puffDetails,
+      {
+        headers: { Authorization: tokenAuth }
+      },
+      {
+        onUploadProgress: uploadEvent => {
+          console.log(
+            "Upload progress: " +
+              Math.round((uploadEvent.loaded / uploadEvent.total) * 100) +
+              "%"
+          );
+        }
+      }
+    );
   }
 };
