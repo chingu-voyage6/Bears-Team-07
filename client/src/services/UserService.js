28525baf7606068a7e97717d44ba3b1a25419691
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2018-08-06 13:56:40 -0500

    resolved merge issues

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -2,15 +2,9 @@ import Api from "./Api.js";
 
 export default {
   readUserData(userId, userToken) {
-    let tokenAuth = "Bearer " + userToken;
-    return Api().get("/api/v1/users/" + userId, {
-      headers: { Authorization: tokenAuth }
-    });
+    return Api(userToken).get("/api/v1/users/" + userId);
   },
   updateUser(userId, editedUserObj, userToken) {
-    let tokenAuth = "Bearer " + userToken;
-    return Api().put("/api/v1/users/" + userId, editedUserObj, {
-      headers: { Authorization: tokenAuth }
-    });
+    return Api(userToken).put("/api/v1/users/" + userId, editedUserObj);
   }
 };
