8fac8936de49d98149236c54ba0111c5f09817a3
Fernando Ortiz <nandub@users.noreply.github.com>
2018-08-05 20:22:13 -0500

    client enhancements and heroku changes (#51)

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -101,7 +101,7 @@ export default {
     frameUrl(imageUrl) {
       if (imageUrl) {
         const url = imageUrl.replace(/\\/g, "/");
-        return process.env.VUE_APP_BACKEND_API_URL + url;
+        return process.env.VUE_APP_BACKEND_API_URL + "/" + url;
       }
     },
     removeAttachment() {
